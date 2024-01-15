f58b8cf08e3df0016b46b6e90c8fd2995fd90989
mingang211 <a989977@naver.com>
2024-01-15 16:21:41 +0900

    Feat  : 추가 starhub 조회기능 추가, api stathubs로 수정

--- src/main/java/com/sparta/devstar_be/hub/controller/HubController.java
+++ src/main/java/com/sparta/devstar_be/hub/controller/HubController.java
@@ -1,11 +1,14 @@
 package com.sparta.devstar_be.hub.controller;
 
 import com.sparta.devstar_be.board.dto.BoardResponseDto;
+import com.sparta.devstar_be.board.service.BoardService;
 import com.sparta.devstar_be.hub.dto.HubResponseDto;
 import com.sparta.devstar_be.hub.service.HubService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -14,14 +17,27 @@ import java.util.List;
 public class HubController {
 
     private final HubService hubService;
+    private final BoardService boardService;
 
-    public HubController(HubService hubService) {
+    public HubController(HubService hubService, BoardService boardService) {
         this.hubService = hubService;
+        this.boardService = boardService;
     }
 
-    @GetMapping("/starhub")
+    @GetMapping("/starhubs")
     public List<HubResponseDto> getHub() {
         return hubService.gethub();
     }
+
+    @GetMapping("/starhubs/{boardId}")
+    public BoardResponseDto getBoard(@PathVariable Long boardId){
+        return boardService.getBoard(boardId);
+    }
+
+//    @GetMapping("/starhus/{boardId}")
+//    public String getBoard(@PathVariable Long boardId, RedirectAttributes attributes) {
+//        attributes.addAttribute("boardId", boardId);
+//        return "redirect:/api/starboards/{boardId}";
+//    }
 
 }
